944505e4ada6254eafe0650da589687f2dc4434f
asadokhin <asadokhin@borlas.ru>
2019-05-23 17:53:54 +0300

    Print Method

--- hw05-gc/src/main/java/ru/otus/gc/GcStatistics.java
+++ hw05-gc/src/main/java/ru/otus/gc/GcStatistics.java
@@ -2,24 +2,31 @@ package ru.otus.gc;
 
 import com.sun.management.GarbageCollectionNotificationInfo;
 
+import javax.management.ListenerNotFoundException;
 import javax.management.NotificationEmitter;
 import javax.management.NotificationListener;
 import javax.management.openmbean.CompositeData;
 import java.lang.management.GarbageCollectorMXBean;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LongSummaryStatistics;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GcStatistics {
     private final BrenchmarkProcess process;
     private List<GarbageCollectionNotificationInfo> notificationInfoList;
+    private boolean isOverLoad = false;
 
     public GcStatistics(BrenchmarkProcess process) {
         this.process = process;
         this.notificationInfoList = new ArrayList<>();
 
+
     }
 
-    private void gatherNotificatioInfo(){
+    private void gatherNotificatioInfo() throws ListenerNotFoundException {
+        System.out.println("Gathering GC Notification info...");
         List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
         for (GarbageCollectorMXBean gcbean : gcbeans) {
             NotificationEmitter emitter = (NotificationEmitter) gcbean;
@@ -30,22 +37,58 @@ public class GcStatistics {
                     notificationInfoList.add(info);
                 }
             };
+
             emitter.addNotificationListener(listener, null, null);
+            //
+            if (isOverLoad) emitter.removeNotificationListener(listener);
         }
     }
 
-    public void start(){
-        gatherNotificatioInfo();
+    public void start() {
         try {
+            gatherNotificatioInfo();
+        } catch (ListenerNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        long startTime = System.currentTimeMillis();
+        try {
+
             process.run();
+        } catch (OutOfMemoryError err) {
+            isOverLoad = true;
+            System.out.println("Memory overload. Worked time, sec: " + (System.currentTimeMillis() - startTime) / 1000);
         }
-        catch (OutOfMemoryError err)
-        {
 
-        }
-        System.out.println("info:" +notificationInfoList.size());
-        notificationInfoList.forEach(info -> System.out.println(info));
+        printNotificationReport();
 
+    }
+
+    private void printNotificationReport() {
+        List<GarbageCollectionNotificationInfo> printList = new ArrayList();
+
+        printList.addAll(notificationInfoList);
+
+        //printList.forEach(info -> info.getGcInfo().System.out.println("GcName: " + info.getGcName() + "GcInfo: " + info.getGcInfo() + "GcAction: " + info.getGcAction() + "GcCause: " + info.getGcCause()));
+
+        System.out.println("Отчет");
+
+        Map<String, List<GarbageCollectionNotificationInfo>> infoByGcName
+                = printList.stream().collect(Collectors.groupingBy(GarbageCollectionNotificationInfo::getGcName));
+
+        infoByGcName.forEach((s, iByGcName) ->
+                {
+                    System.out.println(s + ":\n");
+
+                  iByGcName.stream().collect(Collectors.groupingBy(notificationInfo -> notificationInfo.getGcInfo().getDuration(), Collectors.summarizingLong(value -> value.getGcInfo().getDuration())));
+
+                }
+
+        );
+
+        System.out.println("___________________________");
+
+//        printList.stream().collect()
 
     }
 
--- hw05-gc/src/main/java/ru/otus/gc/OverHeatProcess.java
+++ hw05-gc/src/main/java/ru/otus/gc/OverHeatProcess.java
@@ -1,36 +1,26 @@
 package ru.otus.gc;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class OverHeatProcess implements BrenchmarkProcess {
-
+    List<Object> list = new ArrayList<>();
+    int size = 100000;//9000;
 
     @Override
     public void run() throws OutOfMemoryError {
-        int cnt = 0;
-        int size = 2500000;
-
         while (true) {
-            cnt++;
-            List<String> stringList = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                stringList.add("A");
+            for (int i = 0; i < size; i++) list.add(new Object());
 
-            }
+            list.subList(0, size/2).clear();
+
+
             try {
-                Thread.sleep(3);
+                Thread.sleep(350);
             } catch (InterruptedException e) {
 
             }
-
-            for (int i = 0; i < size/4; i++) {
-                stringList.remove(i);
-
-
-            }
-         /*   for (int i = 0; i < size / 3; i++)
-                stringList.remove(i);*/
 
 
         }
