849a6c3e2acb4f928de7f095a28b09c647444249
Alexander <avsadokhin@yandex.ru>
2019-08-10 20:42:26 +0300

    First release

--- hw14-jmm/src/main/java/TwoThreads.java
+++ hw14-jmm/src/main/java/TwoThreads.java
@@ -1,44 +1,52 @@
 import java.util.concurrent.TimeUnit;
 
 public class TwoThreads {
-    private String msg;
     private static String turn;
+    private final int MIN_COUNT = 1;
+    private final int MAX_COUNT = 10;
 
 
-    public TwoThreads() {
-
+    public static void main(String[] args) {
+        TwoThreads twoThreads = new TwoThreads();
+        twoThreads.go();
     }
 
-    public void go() {
+    private void go() {
         Thread thread1 = new Thread(this::run);
         Thread thread2 = new Thread(this::run);
         thread1.setName("1");
-
         thread2.setName("2");
+
         thread1.start();
         thread2.start();
     }
 
-    public void run() {
-        boolean isCounted;
-
-        for (int i = 1; i <= 10; i++) {
-            isCounted = false;
-            while (true) {
-
-                isCounted = playTurn(i);
-                if (isCounted) break;
+    private void run() {
+        while (true) {
+            for (int i = MIN_COUNT; i <= MAX_COUNT; i++) {
+                makeCount(i);
             }
-
+            for (int i = MAX_COUNT - 1; i > MIN_COUNT; i--) {
+                makeCount(i);
+            }
         }
     }
 
-    public synchronized boolean playTurn(int i) {
+    private void makeCount(int cnt) {
+        boolean isCounted;
+        while (true) {
+            isCounted = playTurn(cnt);
+            if (isCounted) break;
+        }
+
+    }
+
+    private synchronized boolean playTurn(int i) {
         if (!Thread.currentThread().getName().equals(turn)) {
             turn = Thread.currentThread().getName();
             System.out.println("Tread " + turn + ": " + i);
             try {
-                Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -47,10 +55,5 @@ public class TwoThreads {
         return false;
     }
 
-    public static void main(String[] args) {
-        TwoThreads twoThreads = new TwoThreads();
-        twoThreads.go();
 
-
-    }
 }
